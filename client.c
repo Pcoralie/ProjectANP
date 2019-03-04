1223b41b7e4d7d0f9ebc46d65b979ddd0b3b58ce
Pcoralie <Pcoralie@users.noreply.github.com>
2019-03-04 18:22:43 +0100

    last update

--- client.c
+++ client.c
@@ -11,6 +11,7 @@ typedef int socklen_t;
 int main(int argc, char *argv[])
 {
 	WSADATA wsa; 
+	SOCKET s; 
 	
 	printf("\nInitialising Winsock..."); 
 	if (WSAStartup(MAKEWORD(2,2), &wsa) != 0)
@@ -19,7 +20,13 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	
-	printf("Initialised."); 
+	printf("Initialised.\n"); 
+	if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d" , WSAGetLastError());
+		return 1;
+	}
+	printf("Socket created.\n");
 	
 	return 0; 
 }
