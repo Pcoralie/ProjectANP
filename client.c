377e6269f97027899426868ce61faf8365b08700
Pcoralie <Pcoralie@users.noreply.github.com>
2019-03-17 18:55:34 +0100

    update

--- client.c
+++ client.c
@@ -5,10 +5,6 @@
 #include <windef.h>
 #include <winsock2.h>
 typedef int socklen_t;
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-
 
 #elif defined (linux)
 
@@ -34,6 +30,12 @@ typedef struct in_addr IN_ADDR;
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include<io.h>
+
+
 
 int main(int argc, char *argv[])
 {
@@ -42,8 +44,16 @@ int main(int argc, char *argv[])
 #endif
 
 	SOCKET s;
+	SOCKET new_socket;
+
 	// Connect to a server
-	struct sockaddr_in server;
+	struct sockaddr_in server, client;
+	int c; 
+
+	char* client_ip = inet_ntoa(client.sin_addr);
+	int client_port = ntohs(client.sin_port);
+	char* message;
+
 
 
 
@@ -69,9 +79,11 @@ int main(int argc, char *argv[])
 	printf("Socket created.\n");
 
 
-	server.sin_addr.s_addr = inet_addr("172.217.17.35"); //google.fr
+//	server.sin_addr.s_addr = inet_addr("127.0.0.1"); // or localhost to test
+	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_family = AF_INET; // The Internet Protocol version 4 (IPv4)
-	server.sin_port = htons( 80 ); // it would be a good idea to read Course Port Number
+	server.sin_port = htons( 8888 );
+
 	//Connect to remote server
 	if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
 	{
@@ -80,7 +92,99 @@ int main(int argc, char *argv[])
 	}
 	puts("Connected");
 
-	while (getchar() != '\n') ;
+	//Bind
+	if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d" , WSAGetLastError());
+	}
+	puts("Bind done");
+
+	//Listen to incoming connections
+	listen(s , 3);
+
+	// Accept and incoming connection
+	puts("Waiting for incoming connections...");
+
+	c = sizeof(struct sockaddr_in);	//new_socket = accept(s , (struct sockaddr *)&client, &c);
+	/*
+	if (new_socket == INVALID_SOCKET)
+	{
+		printf("accept failed with error code : %d" , WSAGetLastError());
+	}
+	puts("Connection accepted");
+
+	//Reply to client
+	message = "Hello Client , I have received your connection. BYE \n";
+	send(new_socket , message , strlen(message) , 0);
+	getchar();
+	*/
+
+	// server running non stop
+	while( (new_socket = accept(s , (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
+	{
+		puts("Connection accepted");
+
+		//Reply to the client
+		message = "Hello Client , I have received your connection. \n";
+		send(new_socket , message , strlen(message) , 0);
+	}
+
+	/*
+	//Connect to remote server
+	if (connect(s , (struct sockaddr *)&server , sizeof(server)) < 0)
+	{
+		puts("connect error");
+		return 1;
+	}
+	puts("Connected");
+
+	//Send some data
+	message = "GET / HTTP/1.1\r\n\r\n";
+	if( send(s , message , strlen(message) , 0) < 0)
+	{
+		puts("Send failed");
+		return 1;
+	}
+	puts("Data Send\n");
+
+	//Receive a reply from the server
+	if((recv_size = recv(s , server_reply , 2000 , 0)) == SOCKET_ERROR)
+	{
+		puts("recv failed");
+	}
+	puts("Reply received\n");
+
+	//Add a NULL terminating character to make it a proper string before printing
+	server_reply[recv_size] = '\0';
+	puts(server_reply);
+
+
+
+	 if ( (he = gethostbyname( hostname ) ) == NULL)
+	{
+		//gethostbyname failed
+		printf("gethostbyname failed : %d" , WSAGetLastError());
+		return 1;
+	}
+
+	//Cast the h_addr_list to in_addr , since h_addr_list also has the ip address in long format only
+	addr_list = (struct in_addr **) he->h_addr_list;
+
+	for(i = 0; addr_list[i] != NULL; i++)
+	{
+		//Return the first one;
+		strcpy(ip , inet_ntoa(*addr_list[i]) );
+	}
+	printf("%s resolved to : %s\n" , hostname , ip);
+	 */
+
+	closesocket(s); // SOCKET s; for our program sample
+	WSACleanup();
+
+
+	//while (getchar() != '\n') ;
+	printf("\nPress Any Key to Continue\n");
+	getchar();
 	return 0;
 }
 
